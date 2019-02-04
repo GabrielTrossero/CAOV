3558bb469eeeae6944e3ac5b0fc7ace7f66ad2ad
FranciscoDreher <dreherfrancisco@gmail.com>
2019-02-04 17:41:30 -0300

    cambio de extends a master e idioma en vistas auth

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,21 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifique su dirección de correo electrónico') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Se ha enviado un link de verificaión a su e-mail.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('Antes de continuar, chequee su e-mail por el link de verificación.') }}
+                    {{ __('Si no recibió el email') }}, <a href="{{ route('verification.resend') }}">{{ __('click aquí para solicitar otro') }}</a>.
                 </div>
             </div>
         </div>
