29c5f9d18b60332438729755654e8a5c9667eeed
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2019-02-16 19:05:37 -0300

    agregado de links footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <footer>
   CAOV Copyright &copy; 2019 - Club Atlético Oro Verde - Todos los derechos reservados
 	<br/>
-  <i class="fab fa-facebook-square">&nbsp;</i> Club Atlético Oro Verde &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  <i class="fas fa-envelope">&nbsp;</i> caov_@hotmail.com
+  <i class="icono fab fa-facebook-square"></i> <a href="https://www.facebook.com/caovoficial/"> Club Atlético Oro Verde </a>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <i class="icono fas fa-envelope"></i> <a href="mailto:caov_@hotmail.com"> caov_@hotmail.com </a>
 </footer>
