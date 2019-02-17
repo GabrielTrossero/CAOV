808758acfee7e1128570bd2c72adb40253521bf9
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2019-02-17 11:45:30 -0300

    se agregó funcionalidad al menu con js

--- public/js/menu-desplegable.js
+++ public/js/menu-desplegable.js
@@ -1,9 +1,22 @@
-//para el boton de abrir el menu
+//para el boton de abrir o cerrar el menu
 $('#mostrar-nav').on('click',function(){
   $('nav').toggleClass('mostrar');
+  if($('li').hasClass('abierto')){//para que el menú se "reinicie" cuado lo cierro
+    $('li').children('.children').slideUp();
+    $('li').removeClass('abierto');
+  }
 });
 
-//para abrir o cerrar el submenu
+
+
+//funcion que se llama cuando hago click
 $('.submenu').click(function(){
+  if((!$(this).hasClass('abierto')) && ($('li').hasClass('abierto'))){//para cerrar un submenu abierto cuado quiero abrir otro
+    $('li').children('.children').slideUp();
+    $('li').removeClass('abierto');
+  }
+
+  //para cerrar o abrir un submenu cuando hago click
 	$(this).children('.children').slideToggle();
+  $(this).addClass('abierto');
 });
