23dad82b17497b972c799c1c81a1708fc346a136
GabrielTrossero <46575291+GabrielTrossero@users.noreply.github.com>
2019-02-25 09:29:45 -0300

    Update menu.blade.php

--- resources/views/pagoAlquiler/menu.blade.php
+++ resources/views/pagoAlquiler/menu.blade.php
@@ -4,7 +4,7 @@
 
 <div class="menu2">
   <a href="{{ url('pagoalquiler/listamueble') }}"><i class="icono3 fas fa-chair"></i> &nbsp; Lista de Alquileres de Muebles</a>
-  <a href="{{ url('pagoalquiler/listainmueble') }}"><i class="icono3 fas fa-ethernet"></i><&nbsp; Lista de Alquileres de Inmuebles</a>
+  <a href="{{ url('pagoalquiler/listainmueble') }}"><i class="icono3 fas fa-ethernet"></i>&nbsp; Lista de Alquileres de Inmuebles</a>
 </div>
 
 
