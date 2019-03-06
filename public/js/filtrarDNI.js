d9671eeff289efcc5b6083b62115b969d7feca38
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2019-03-06 10:18:23 -0300

    agregado de comentarios en filtrarDNI.js

--- public/js/filtrarDNI.js
+++ public/js/filtrarDNI.js
@@ -7,6 +7,9 @@
  * el input debe tener id -> filtroDNI
  * la tabla debe tener id -> tablaFiltroDNI
  */
+
+
+
 //  CON JavaScript PURO
 /*
 //capturo el elemento con tal id
@@ -47,26 +50,30 @@ input.addEventListener('keyup', function(event) {
 });
 */
 
+
+
+
+
 // CON jQuery
 
-var input = $("#filtroDNI");
-    input.keyup(() => {
+var input = $("#filtroDNI"); //capturo el evento
+    input.keyup(() => { //funcion que se ejecuta cada vez que se desencadena el evento
         var td, textValue;
-        var filter = input.val();
-        filter.toUpperCase();
+        var filter = input.val(); //le asigno el valor ingresado a la variable
+        filter.toUpperCase(); //paso la cadena a mayusculas
 
-        var tr = $("#tablaFiltroDNI tr");
+        var tr = $("#tablaFiltroDNI tr"); //capturo todos los tr que estan dentro de la tabla
 
         for (var i = 1; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
+            td = tr[i].getElementsByTagName("td")[0]; //captura por ej <td>36987425</td>
 
             if (td) {
-                textValue = td.textContent || td.innerText;
+                textValue = td.textContent || td.innerText; //capturo solo el valor sacando los "td"
 
-                if (textValue.toUpperCase().indexOf(filter) == 0) {
-                    tr[i].style.display = '';
+                if (textValue.toUpperCase().indexOf(filter) == 0) { //si coinciden es 0
+                    tr[i].style.display = ''; //lo muestro
                 } else {
-                    tr[i].style.display = 'none';
+                    tr[i].style.display = 'none'; //lo oculto
                 }
             }
         }
