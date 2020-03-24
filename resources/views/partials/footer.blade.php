ef60237fa303ef8dd6418fcad7b5aad6c6ec8993
FranciscoDreher <dreherfrancisco@gmail.com>
2020-03-24 18:51:15 -0300

    agregado detalle de contacto a desarrolladores en footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,4 +4,6 @@
   <i class="icono fab fa-facebook-square"></i> <a class="link1" href="https://www.facebook.com/caovoficial/"> Club Atlético Oro Verde </a>
   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <i class="icono fas fa-envelope"></i> <a class="link1" href="mailto:caov_@hotmail.com"> caov_@hotmail.com </a>
+  <br>
+  Desarrollado por <a class="link1" href="mailto:dreherfrancisco@gmail.com">Dreher</a>{{ " - " }}<a class="link1" href="mailto:juliotrillobaron@gmail.com ">Trillo</a>{{ " - " }}<a class="link1" href="mailto:gabrieltrosserogetr@gmail.com ">Trossero</a>
 </footer>
