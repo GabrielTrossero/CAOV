08b13feec0d270993e06526e9b6d0ce3a373416f
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2020-08-17 14:46:34 -0300

    cambio en el footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
-  CAOV Copyright &copy; 2019 - Club Atlético Oro Verde - Todos los derechos reservados
+  CAOV Copyright &copy; 2020 - Club Atlético Oro Verde - Todos los derechos reservados
 	<br/>
   <i class="icono fab fa-facebook-square"></i> <a class="link1" href="https://www.facebook.com/caovoficial/"> Club Atlético Oro Verde </a>
   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <i class="icono fas fa-envelope"></i> <a class="link1" href="mailto:caov_@hotmail.com"> caov_@hotmail.com </a>
   <br>
-  Desarrollado por <a class="link1" href="mailto:dreherfrancisco@gmail.com">Dreher</a>{{ " - " }}<a class="link1" href="mailto:juliotrillobaron@gmail.com ">Trillo</a>{{ " - " }}<a class="link1" href="mailto:gabrieltrosserogetr@gmail.com ">Trossero</a>
+  Desarrollado por <a class="link1" href="mailto:dreherfrancisco@gmail.com">Dreher</a>{{ " - " }}<a class="link1" href="mailto:juliotrillobaron@gmail.com ">Trillo</a>{{ " - " }}<a class="link1" href="mailto:gabrieltrosserogetr@gmail.com ">Trossero</a> &nbsp; {{ " | "}} &nbsp; {{"Taller de Integración - FCyT UADER" }}
 </footer>
