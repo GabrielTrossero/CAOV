465053bf8de56a3aca382ebe818084d6b6948852
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2020-11-03 15:22:37 -0300

    cambiado link libreria js del boton del menu desplegable

--- resources/views/partials/menuDesplegable.blade.php
+++ resources/views/partials/menuDesplegable.blade.php
@@ -1,6 +1,6 @@
 @auth
 
-  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
   <div id="mostrar-nav">
     <i class="icono2 fas fa-bars"></i>
